07b3f5e880d3d562d998f933dfefaaed98327c0b
Vladimir Levin <vmpstr@chromium.org>
2019-07-16 00:17:57 -0400

    figured out tests? and added basic rect tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["js", "json", "jsx", "node", "mjs"],
+  transformIgnorePatterns: [
+    "node_modules/"
+  ],
+  transform : {
+    "^.+\\.m?[t|j]sx?$": "babel-jest"
+  }
+}
