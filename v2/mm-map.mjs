37e19b49c98ae10c50dfc310ff98559942c55461
Vladimir Levin <vmpstr@chromium.org>
2020-01-06 00:03:49 -0500

    Making progress.
    - Renamed test2.html to v2.html
    - Finished drag
    - Finished label edit
    - Finished adding and removing nodes
    
    Left to do from the initial list:
    - Save / persist

--- v2/mm-map.mjs
+++ v2/mm-map.mjs
@@ -1,8 +1,14 @@
 window.customElements.define("mm-map", class extends HTMLElement {
   #nodes
+  #selectedNode = null;
 
   constructor() {
     super();
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot)
+      return;
 
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML = `
@@ -22,21 +28,77 @@ window.customElements.define("mm-map", class extends HTMLElement {
       const slot = shadow.querySelector("slot");
       slot.addEventListener("slotchange", this.#onSlotChange);
       this.#onSlotChange();
+      this.#doInitialLayout();
     });
+
+    this.addEventListener("click", (e) => {
+      this.#onClick(e);
+    });
+
+    this.addEventListener("dblclick", (e) => {
+      this.#onDoubleClick(e);
+    });
+
+    this.addEventListener
   }
 
   #onSlotChange = (e) => {
     let nodes = this.shadowRoot.querySelector("slot").assignedNodes();
-    let y = 0;
     this.#nodes = [];
     for (let i = 0; i < nodes.length; ++i) {
       if (nodes[i].tagName && nodes[i].tagName.toLowerCase() === "mm-node") {
         this.#nodes.push(nodes[i]);
         nodes[i].setMap(this);
-        nodes[i].style.top = y + "px";
-        nodes[i].style.left = "0px";
-        y += nodes[i].getBoundingClientRect().height;
       }
+    }
+  }
+
+  #doInitialLayout = () => {
+    let y = 0;
+    for (let i = 0; i < this.#nodes.length; ++i) {
+      this.#nodes[i].style.top = y + "px";
+      this.#nodes[i].style.left = "0px";
+      y += this.#nodes[i].getBoundingClientRect().height;
+    }
+  }
+
+  nodeSelected(node) {
+    if (this.#selectedNode)
+      this.#selectedNode.deselect();
+    this.#selectedNode = node;
+  }
+
+  nodeDeselected(node) {
+    if (this.#selectedNode == node)
+      this.#selectedNode = null;
+  }
+
+  #onClick = (e) => {
+    this.#selectedNode && this.#selectedNode.deselect();
+    e.stopPropagation();
+  }
+
+  #onDoubleClick = (e) => {
+    const node = document.createElement("mm-node");
+    node.setMap(this);
+    // Append to the light dom so it's inspectable and gets assigned
+    // to the slot.
+    this.appendChild(node);
+    const rect = node.getBoundingClientRect();
+    node.style.left = (e.clientX - 0.5 * rect.width) + "px";
+    node.style.top = (e.clientY - 0.5 * rect.height) + "px";
+    node.select();
+    node.startLabelEdit();
+    e.stopPropagation();
+  }
+
+  handleKeyDown = (e) => {
+    if (e.key == "Delete" || e.key == "Backspace") {
+      const node = this.#selectedNode;
+      if (!node)
+        return;
+      node.deselect();
+      node.remove();
     }
   }
 });
--- v2/mm-node.mjs
+++ v2/mm-node.mjs
@@ -3,6 +3,11 @@ window.customElements.define("mm-node", class extends HTMLElement {
 
   constructor() {
     super();
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot)
+      return;
 
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML = `
@@ -29,7 +34,24 @@ window.customElements.define("mm-node", class extends HTMLElement {
       <div class=label>new task</div>
     `;
     this.setAttribute("draggable", true);
-    this.addEventListener("dblclick", () => { this.startLabelEdit(); });
+    this.addEventListener("dblclick", (e) => {
+      this.startLabelEdit();
+      e.stopPropagation();
+    });
+    this.addEventListener("click", (e) => {
+      this.select();
+      e.stopPropagation();
+    });
+
+    this.addEventListener("dragstart", (e) => {
+      this.#onDragStart(e);
+    });
+    this.addEventListener("drag", (e) => {
+      this.#onDrag(e);
+    });
+    this.addEventListener("dragend", (e) => {
+      this.#onDragEnd(e);
+    });
   }
 
   setMap = (map) => {
@@ -38,6 +60,7 @@ window.customElements.define("mm-node", class extends HTMLElement {
 
   #endLabelEdit = (e) => {
     if (e.type === "keydown") {
+      e.stopPropagation();
       if (e.key !== "Enter") 
         return;
     }
@@ -47,6 +70,7 @@ window.customElements.define("mm-node", class extends HTMLElement {
     // Restore ellipsis if necessary.
     e.target.style.overflow = "";
 
+    e.target.innerText = e.target.innerText.trim();
     // If we have nothing, keep the height with zero-width space.
     if(e.target.innerText == "")
       e.target.innerHTML = '&#x200b;'
@@ -82,5 +106,32 @@ window.customElements.define("mm-node", class extends HTMLElement {
     // Add event listeners so we can stop editing.
     el.addEventListener("keydown", this.#endLabelEdit);
     el.addEventListener("focusout", this.#endLabelEdit);
+  }
+
+  select = () => {
+    this.#map.nodeSelected(this);
+    this.style.borderColor = "blue";
+  }
+
+  deselect = () => {
+    this.#map.nodeDeselected(this);
+    this.style.borderColor = "";
+  }
+
+  #dragOffset = [0, 0];
+  #onDragStart = (e) => {
+    const rect = this.getBoundingClientRect();
+    this.#dragOffset[0] = rect.x - e.clientX;
+    this.#dragOffset[1] = rect.y - e.clientY;
+    this.style.opacity = "20%";
+  }
+  #onDrag = (e) => {
+    if (e.clientX == 0 && e.clientY == 0)
+      return;
+    this.style.left = (this.#dragOffset[0] + e.clientX) + "px";
+    this.style.top = (this.#dragOffset[1] + e.clientY) + "px";
+  }
+  #onDragEnd = (e) => {
+    this.style.opacity = "";
   }
 });
