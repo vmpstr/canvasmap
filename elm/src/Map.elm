9569a4361abe1040ffb9570d211f883135ebdd2b
vmpstr <vmpstr@chromium.org>
2020-09-01 00:01:27 -0400

    Custom draggable

--- elm/src/Map.elm
+++ elm/src/Map.elm
@@ -4,12 +4,15 @@ import Debug
 import Browser
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class, style)
-import Html.Events exposing (onMouseDown)
+import Html.Events exposing (on)
+import Json.Decode as Decoder exposing (Decoder, succeed, int, string, float)
+import Json.Decode.Pipeline exposing (required, optional, hardcoded)
 
 type Msg
   = MsgNoop
-  | MsgOnDrag DragData
-  | MsgOnMouseDown String
+  | MsgOnDrag OnDragData
+  | MsgOnPointerDown OnPointerDownPortData
+
 
 type alias Vector = { x : Float, y : Float }
 type alias Rect =
@@ -17,10 +20,10 @@ type alias Rect =
   , size : Vector
   }
 
-type alias DragData =
+type alias OnDragData =
   { targetId : String
-  , delta : Vector
-  , geometry : Maybe Geometry
+  , dx : Float
+  , dy : Float
   }
 
 type alias Geometry =
@@ -61,7 +64,34 @@ asPx : Float -> String
 asPx n =
   String.fromInt (round n) ++ "px"
 
-port portOnMouseDown : String -> Cmd msg
+toMsgOrNoop : (data -> Msg) -> Result err data -> Msg
+toMsgOrNoop toMsg result =
+  case result of
+      Ok data ->
+        toMsg data
+
+      Err _ ->
+        MsgNoop
+
+
+type alias OnPointerDownPortData =
+  { targetId : String
+  , pointerType : String
+  , x : Float
+  , y : Float
+  }
+
+onPointerDownDecoder : String -> Decoder Msg
+onPointerDownDecoder targetId =
+  Decoder.map MsgOnPointerDown 
+    (succeed OnPointerDownPortData
+      |> hardcoded targetId
+      |> required "pointerType" string
+      |> required "clientX" float
+      |> required "clientY" float)
+
+port portOnPointerDown : OnPointerDownPortData -> Cmd msg
+port portOnDrag : (Decoder.Value -> msg) -> Sub msg
 
 initModel : Model
 initModel =
@@ -72,20 +102,26 @@ initModel =
                  , size = { x = 200, y = 50 }
                  , children = Children []
                  }
+               , { id = "e2"
+                 , nodeType = TopLevel
+                 , position = { x = 300, y = 20 }
+                 , size = { x = 200, y = 50 }
+                 , children = Children []
+                 }
                ]
   }
 
 view : Model -> Html Msg
 view model =
   div [ class "map" ]
-      (List.map viewNode (childList model.nodes))
+      (childList model.nodes |> List.map viewNode)
 
 
 viewNode : Node -> Html Msg
 viewNode node =
   case node.nodeType of
     TopLevel ->
-      div [ onMouseDown (MsgOnMouseDown node.id)
+      div [ on "pointerdown" (onPointerDownDecoder node.id)
           , class "top_child"
           , style "width" (asPx node.size.x)
           , style "height" (asPx node.size.y)
@@ -96,21 +132,52 @@ viewNode node =
     Child ->
       div [] []
 
+dragPosition : OnDragData -> Children -> Children
+dragPosition { targetId, dx, dy } (Children nodes) =
+  let 
+    updatePosition : Vector -> Vector
+    updatePosition position =
+      { x = position.x + dx, y = position.y + dy }
+
+    updateNode : Node -> Node
+    updateNode node =
+      if node.id == targetId then
+        { node | position = updatePosition node.position }
+      else
+        node
+  in
+  Children (List.map updateNode nodes)
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    MsgOnMouseDown id ->
-      (model, portOnMouseDown id)
+    MsgOnPointerDown data ->
+      (model, portOnPointerDown data )
+
+    MsgOnDrag data ->
+      ({ model | nodes = dragPosition data model.nodes }, Cmd.none)
 
     _ ->
-
       (model, Cmd.none)
 
 init : () -> (Model, Cmd Msg)
 init () = (initModel, Cmd.none)
 
+onDragDecoder : Decoder OnDragData
+onDragDecoder =
+  succeed OnDragData
+    |> required "targetId" string
+    |> required "dx" float
+    |> required "dy" float
+
+onDragSubscription : Model -> Sub Msg
+onDragSubscription model =
+  portOnDrag (Decoder.decodeValue onDragDecoder >> (toMsgOrNoop MsgOnDrag))
+
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+  [ onDragSubscription model
+  ] |> Sub.batch
 
 main : Program () Model Msg
 main =
