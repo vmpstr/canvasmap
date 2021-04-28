0eb7f0dfb625543c2326a480ef51553f0e5d144d
Vladimir Levin <vmpstr@chromium.org>
2021-04-27 23:24:29 -0400

    clean up resize

--- purs/src/App/Control/Resize.purs
+++ purs/src/App/Control/Resize.purs
@@ -14,7 +14,7 @@ import Data.Map as Map
 
 import Web.Event.Event (stopPropagation, currentTarget)
 import Web.UIEvent.MouseEvent (MouseEvent, clientX, clientY, toEvent)
-import Web.HTML.HTMLElement (HTMLElement, fromElement, getBoundingClientRect)
+import Web.HTML.HTMLElement (HTMLElement, fromElement, getBoundingClientRect, DOMRect)
 import Web.DOM.Element (closest, fromEventTarget, toParentNode)
 import Web.DOM.ParentNode (QuerySelector(..), querySelector)
 
@@ -48,88 +48,6 @@ handleAction action state =
       else
         pure $ state /\ SCT.NoChange
 
-setNodeMaxWidth :: MS.State -> NodeId -> Maybe Number -> MS.State
-setNodeMaxWidth state id value =
-  state
-    { nodes = Map.update
-        (\node -> Just $ Node.setMaxWidth node value)
-        id
-        state.nodes
-    }
-
-setNodeMaxHeight :: MS.State -> NodeId -> Maybe Number -> MS.State
-setNodeMaxHeight state id value =
-  state
-    { nodes = Map.update
-        (\node -> Just $ Node.setMaxHeight node value)
-        id
-        state.nodes
-    }
-
-getResizeState :: MS.State -> Maybe RS.State
-getResizeState state =
-  case state.mode of
-    MM.Resize rstate -> Just rstate
-    _ -> Nothing
-
-updateSize :: MouseEvent -> MS.State -> MS.State
-updateSize me state = fromMaybe state do -- Maybe
-  rstate <- getResizeState state
-  let
-    dx = (toNumber $ clientX me) - rstate.x
-    dy = (toNumber $ clientY me) - rstate.y
-    width = rstate.width + dx
-    height = rstate.height + dy
-    -- Update the RS state
-    rstate' = rstate
-                { x = rstate.x + dx
-                , y = rstate.y + dy
-                , width = width
-                , height = height
-                }
-    -- Update MS state with the RS state
-    state' = state { mode = MM.Resize rstate' }
-  -- Set the new max width on the node
-  let widthState = if affectsWidth rstate.direction then
-                     setNodeMaxWidth state' rstate.id (Just width)
-                   else state'
-  let finalState = if affectsHeight rstate.direction then
-                     setNodeMaxHeight widthState rstate.id (Just height)
-                   else widthState
-  pure finalState
-
-affectsWidth :: RS.Direction -> Boolean
-affectsWidth RS.EW = true
-affectsWidth _ = false
-
-affectsHeight :: RS.Direction -> Boolean
-affectsHeight RS.NS = true
-affectsHeight _ = false
-
-stopResize :: forall m. MonadEffect m => MouseEvent -> MS.State -> m MS.State
-stopResize me state =
-  case state.mode of
-    MM.Resize rstate -> do
-      let state' = state { mode = MM.Idle }
-      let mroot = (currentTarget $ toEvent me) >>= fromEventTarget
-      mresizer <- liftEffect $ case mroot of
-                    Just root -> querySelector (QuerySelector ".resized") $ toParentNode root
-                    Nothing -> pure Nothing
-      case mresizer >>= fromElement of
-        Just htmlElement -> do
-          rect <- liftEffect $ getBoundingClientRect htmlElement
-          widthState <-
-            if (affectsWidth rstate.direction) && (rect.width + 3.0) < rstate.width then do
-              pure $ setNodeMaxWidth state' rstate.id Nothing
-            else
-              pure state'
-          if (affectsHeight rstate.direction) && (rect.height + 3.0) < rstate.height then do
-            pure $ setNodeMaxHeight widthState rstate.id Nothing
-          else
-            pure widthState
-        Nothing -> pure state'
-    _ -> pure state
-
 startEWResize :: forall m.
   Log.Logger m => MonadEffect m => MonadPlus m =>
   MouseEvent -> NodeId -> MS.State -> m MS.State
@@ -140,10 +58,6 @@ startNSResize :: forall m.
   MouseEvent -> NodeId -> MS.State -> m MS.State
 startNSResize = startResizeFromSelectionContainer RS.NS
 
-liftMaybe :: forall m a. MonadPlus m => Maybe a -> m a
-liftMaybe (Just x) = pure x
-liftMaybe Nothing = empty
-
 startResizeFromSelectionContainer :: forall m.
   Log.Logger m => MonadEffect m => MonadPlus m =>
   RS.Direction -> MouseEvent -> NodeId -> MS.State -> m MS.State
@@ -153,6 +67,7 @@ startResizeFromSelectionContainer direction me id state = do
     node <- wrap $ closest (QuerySelector ".selection_container") resizer
     liftMaybe $ fromElement node
   maybe (pure state) startResizeFromElement melement
+
   where
   startResizeFromElement :: HTMLElement -> m MS.State
   startResizeFromElement element = do
@@ -168,3 +83,105 @@ startResizeFromSelectionContainer direction me id state = do
         , id: id
         }
       }
+
+updateSize :: MouseEvent -> MS.State -> MS.State
+updateSize me state = resizeMode state \rstate ->
+  let
+    dx = (toNumber $ clientX me) - rstate.x
+    dy = (toNumber $ clientY me) - rstate.y
+    width = rstate.width + dx
+    height = rstate.height + dy
+    -- Update the RS state
+    rstate' = rstate
+                { x = rstate.x + dx
+                , y = rstate.y + dy
+                , width = width
+                , height = height
+                }
+    -- Update MS state with the RS state
+    state' = state { mode = MM.Resize rstate' }
+    -- Update node dimensiosn
+    widthState = updateNodeWidthIfNeeded rstate' state'
+    finalState = updateNodeHeightIfNeeded rstate' widthState
+  in
+  finalState
+
+  where
+  updateNodeWidthIfNeeded :: RS.State -> MS.State -> MS.State
+  updateNodeWidthIfNeeded rs s =
+    if affectsWidth rs.direction then
+      setNodeMaxWidth s rs.id (Just rs.width)
+    else s
+
+  updateNodeHeightIfNeeded :: RS.State -> MS.State -> MS.State
+  updateNodeHeightIfNeeded rs s =
+    if affectsHeight rs.direction then
+      setNodeMaxHeight s rs.id (Just rs.height)
+    else s
+
+stopResize :: forall m. MonadEffect m => MouseEvent -> MS.State -> m MS.State
+stopResize me state = resizeModeM state \rstate -> do
+  let state' = state { mode = MM.Idle }
+  melement <- runMaybeT do
+    root <- liftMaybe $ (currentTarget $ toEvent me) >>= fromEventTarget
+    resizer <- wrap $ liftEffect $ querySelector (QuerySelector ".resized") $ toParentNode root
+    liftMaybe $ fromElement resizer
+  maybe (pure state') (stopResize' rstate state') melement
+
+  where
+  stopResize' :: RS.State -> MS.State -> HTMLElement -> m MS.State
+  stopResize' rstate localState element = do
+    rect <- liftEffect $ getBoundingClientRect element
+    let noWidthState = resetWidthIfNeeded rstate localState rect
+    let finalState = resetHeightIfNeeded rstate noWidthState rect
+    pure finalState
+
+  resetWidthIfNeeded :: RS.State -> MS.State -> DOMRect -> MS.State
+  resetWidthIfNeeded rs s r =
+    if affectsWidth rs.direction && r.width + 3.0 < rs.width then
+      setNodeMaxWidth s rs.id Nothing
+    else s
+
+  resetHeightIfNeeded :: RS.State -> MS.State -> DOMRect -> MS.State
+  resetHeightIfNeeded rs s r =
+    if affectsHeight rs.direction && r.height + 3.0 < rs.height then
+      setNodeMaxHeight s rs.id Nothing
+    else s
+
+resizeModeM :: forall m.
+  MonadEffect m =>
+  MS.State -> (RS.State -> m MS.State) -> m MS.State
+resizeModeM state f =
+  case state.mode of
+    MM.Resize rstate -> f rstate
+    _ -> pure state
+
+resizeMode :: MS.State -> (RS.State -> MS.State) -> MS.State
+resizeMode state f =
+  case state.mode of
+    MM.Resize rstate -> f rstate
+    _ -> state
+
+liftMaybe :: forall m a. MonadPlus m => Maybe a -> m a
+liftMaybe (Just x) = pure x
+liftMaybe Nothing = empty
+
+affectsWidth :: RS.Direction -> Boolean
+affectsWidth RS.EW = true
+affectsWidth _ = false
+
+affectsHeight :: RS.Direction -> Boolean
+affectsHeight RS.NS = true
+affectsHeight _ = false
+
+setNodeMaxWidth :: MS.State -> NodeId -> Maybe Number -> MS.State
+setNodeMaxWidth state id value = state
+  { nodes =
+      Map.update (\node -> Just $ Node.setMaxWidth node value) id state.nodes
+  }
+
+setNodeMaxHeight :: MS.State -> NodeId -> Maybe Number -> MS.State
+setNodeMaxHeight state id value = state
+  { nodes =
+      Map.update (\node -> Just $ Node.setMaxHeight node value) id state.nodes
+  }
